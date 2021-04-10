847ad7e2b691f1cea097e97e230ef8e51c3606e3
djtwdix <djtwd.ix@icloud.com>
2021-04-10 17:31:40 -0300

    Create chat controller functions

---
+++ server/controllers/ChatControllers.js
@@ -0,0 +1,23 @@
+import Chats from "../models/Chats.js"
+
+export const createChat = (req, res) => {
+  const chatInfo = req.body
+  Chats.create(chatInfo, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+}
+
+export const getChatsByUserId = (req, res) => {
+  const user_id = req.body
+  Chats.find({participants: user_id}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  })
+}
