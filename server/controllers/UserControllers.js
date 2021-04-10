c7d654413a3db855a572789ebda16d990608d868
djtwdix <djtwd.ix@icloud.com>
2021-04-10 18:32:42 -0300

    Create getUserById for route and rename helper

--- server/controllers/UserControllers.js
+++ server/controllers/UserControllers.js
@@ -1,6 +1,6 @@
 import Users from "../models/UserModel.js"
 
-const getUserById = (userId) => {
+const checkIfUserExists = (userId) => {
   return Users.findById(userId, (err, data) => {
     if (err) {
       return null;
@@ -10,9 +10,20 @@ const getUserById = (userId) => {
   });
 }
 
+export const getUserById = (req, res) => {
+  const user_id = req.params.userId
+  Users.find({_id: user_id}, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+}
+
 export const createUser = async (req, res) => {
   const userInfo = req.body
-  const userExists = await getUserById(req.body._id)
+  const userExists = await checkIfUserExists(req.body._id)
   if (!userExists) {
      Users.create(userInfo, (err, data) => {
       if (err) {
