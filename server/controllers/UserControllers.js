1aaa8c745992ba1de21736ae55b08285b9319752
djtwdix <djtwd.ix@icloud.com>
2021-04-10 13:48:34 -0300

    Create users controller with createUser function

---
+++ server/controllers/UserControllers.js
@@ -0,0 +1,13 @@
+import Users from "../models/UserModel"
+
+export const createUser = async (req, res) => {
+  const userInfo = req.body
+  await Users.create(userInfo, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(201).send(data);
+    }
+  });
+}
+
