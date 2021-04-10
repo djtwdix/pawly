acfc27f2ea2300bbb35c44c61f15187ee3be56f5
djtwdix <djtwd.ix@icloud.com>
2021-04-10 12:40:44 -0300

    import dotenv and connect mongoDB

--- server/server.js
+++ server/server.js
@@ -3,6 +3,9 @@ import http from "http"
 import cors from "cors"
 import mongoose from "mongoose"
 import { Server } from "socket.io"
+import dotenv from 'dotenv';
+dotenv.config()
+
 const PORT = process.env.PORT || 8080
 
 const app = express()
@@ -16,6 +19,17 @@ const io = new Server(httpServer, {
     origin: "http://localhost:3002",
     methods: ["GET", "POST"]
   }
+})
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+
+db.on("open", () => {
+  console.log("Connected to MongoDB")
 })
 
 app.get("/", (req, res) => {
