da2934445688f4ddaf31d42669619590d4b8596b
djtwdix <djtwd.ix@icloud.com>
2021-04-10 13:07:42 -0300

    Create user model

---
+++ server/models/UserModel.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose"
+
+const userModel = mongoose.Schema({
+  _id: String,
+  name: String,
+  email: String,
+  photoURL: String,
+  blocks: Array,
+  likes: Array
+})
+
+export default mongoose.model("users", userModel)
