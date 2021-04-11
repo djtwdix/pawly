fbb483401f381e4000ad0e600594896c4e1bf11b
Jesse <jessedaniel@live.ca>
2021-04-11 11:58:52 -0600

    add pup form component & hook

---
+++ client/src/components/AddPupForm.js
@@ -0,0 +1,126 @@
+import {
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  FormLabel,
+  Input,
+  InputLabel,
+  Radio,
+  RadioGroup,
+  Slider,
+  TextField,
+} from "@material-ui/core";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
+import usePupData from "../hooks/usePupData";
+
+
+
+//Component for for adding new pup
+export default function AddPupForm({ user }) {
+  const { formData, handleChange, handleDateChange, handleEnergyChange, addPup, charRemaining, setCharRemaining} = usePupData();
+  return (
+    <form className="pupForm" onSubmit={addPup}>
+      <FormControl>
+        <InputLabel htmlFor="image"></InputLabel>
+        <Input
+        type="text"
+        name="photoURL"
+        id="image"
+        value={formData.photoURL}
+        aria-describeby="image"
+        onChange={handleChange}
+        />
+      </FormControl>
+      <FormControl>
+        <InputLabel htmlFor="Name"></InputLabel>
+        <Input
+        name="name"
+        id="name"
+        value={formData.name}
+        aria-describeby="name"
+        onChange={handleChange}
+        />
+      </FormControl>
+      <FormControl>
+        <TextField
+        error={charRemaining < 0}
+        multiline
+        label="Bio"
+        name="bio"
+        value={formData.bio}
+        aria-describeby="bio"
+        onChange={handleChange}
+        />
+        <FormHelperText>{charRemaining} characters remaining</FormHelperText>
+      </FormControl>
+      <FormControl component="fieldset">
+        <FormLabel className="pupForm_energy" component="legend">
+          Gender
+        </FormLabel>
+        <RadioGroup
+        value={formData.gender}
+        name="gender"
+        onChange={handleChange}
+        aria-label="gender"
+        row
+        >
+          <FormControlLabel
+          value="female"
+          control={<Radio />}
+          label="female"
+          labelPlacement="start"
+          style={{
+            color: "black"
+          }}
+          />
+          <FormControlLabel
+          value="male"
+          control={<Radio />}
+          label="male"
+          labelPlacement="start"
+          style={{
+            color: "black"
+          }}
+          />
+        </RadioGroup>
+      </FormControl>
+      <InputLabel className="pupForm_energy" htmlFor="energy" mt="3">
+        Energy
+      </InputLabel>
+      <Slider
+      defaultValue={1}
+      id="energy"
+      name="energy"
+      onChange={handleEnergyChange}
+      aria-labelledy="discrete-sldier-custom"
+      step={1}
+      valueLabelDisplay="auto"
+      min={1}
+      max={5}
+      />
+
+      <MuiPickersUtilsProvider utils={MomentUtils}>
+        <KeyboardDatePicker
+        disableToolbar
+        variant="inline"
+        value={selectedDate}
+        name="date"
+        format="MM/DD/yyyy"
+        margin="normal"
+        id="date-picker-inline"
+        label="Birthday"
+        onChange={handleDateChange}
+        KeyboardButtonProps={{
+          "aria-label": "change date",
+        }}
+        />
+      </MuiPickersUtilsProvider>
+      <Button type="submit"></Button>
+      </form>
+  );
+}
---
+++ client/src/hooks/usePupData.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import {useState} from "react";
+
+
+export default function usePupData() {
+  const [selectedDate, setSelectedDate] = useState();
+  const [charRemaining, setCharReamaining] = useState(140);
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+    console.log(formData);
+  };
+
+  const handleDateChange = (e) => {
+    console.log(date);
+    setSelectedDate(date);
+    setFormData({ ...formData, date: date });
+    console.log(formData);
+  };
+
+  const handleEnergyChange = (event, number) => {
+    setFormData({ ...formData, energy: number });
+    console.log(formData);
+  }
+
+  const addPup = (e) => {
+    e.preventDefault();
+    console.log("clicked submit");
+    console.log("doggie data:", formData);
+    return axios.post("/pups", { ...formData, owner_id: user.uid });
+  };
+}
