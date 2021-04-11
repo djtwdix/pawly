194f1ce645a3165a048b75606b47ebe8f0a5659c
djtwdix <djtwd.ix@icloud.com>
2021-04-11 12:57:58 -0300

    Create firebase auth sign in component

---
+++ client/src/components/SignIn.js
@@ -0,0 +1,27 @@
+import React from "react";
+import firebase from "firebase/app";
+import { auth } from "../firebase/config.js";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: "/",
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.EmailAuthProvider.PROVIDER_ID,
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+  callbacks: {
+    signInSuccessWithAuthResult: () => {},
+  },
+};
+
+export default function SignIn() {
+  return (
+    <div>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
+    </div>
+  );
+}
