10176bae72a43ebc463d362dfb41aa3991fac1f8
djtwdix <djtwd.ix@icloud.com>
2021-04-11 15:28:52 -0300

    Destructure selected date to usePupData import

--- client/src/components/AddPupForm.js
+++ client/src/components/AddPupForm.js
@@ -18,44 +18,51 @@ import {
 import MomentUtils from "@date-io/moment";
 import usePupData from "../hooks/usePupData";
 
-
-
 //Component for for adding new pup
 export default function AddPupForm({ user }) {
-  const { formData, handleChange, handleDateChange, handleEnergyChange, addPup, charRemaining, setCharRemaining} = usePupData();
-  
+  const {
+    formData,
+    handleChange,
+    handleDateChange,
+    handleEnergyChange,
+    addPup,
+    charRemaining,
+    setCharRemaining,
+    selectedDate,
+  } = usePupData();
+
   return (
     <form className="pupForm" onSubmit={addPup}>
       <FormControl>
         <InputLabel htmlFor="image"></InputLabel>
         <Input
-        type="text"
-        name="photoURL"
-        id="image"
-        value={formData.photoURL}
-        aria-describeby="image"
-        onChange={handleChange}
+          type="text"
+          name="photoURL"
+          id="image"
+          value={formData.photoURL}
+          aria-describedby="image"
+          onChange={handleChange}
         />
       </FormControl>
       <FormControl>
         <InputLabel htmlFor="Name"></InputLabel>
         <Input
-        name="name"
-        id="name"
-        value={formData.name}
-        aria-describeby="name"
-        onChange={handleChange}
+          name="name"
+          id="name"
+          value={formData.name}
+          aria-describedby="name"
+          onChange={handleChange}
         />
       </FormControl>
       <FormControl>
         <TextField
-        error={charRemaining < 0}
-        multiline
-        label="Bio"
-        name="bio"
-        value={formData.bio}
-        aria-describeby="bio"
-        onChange={handleChange}
+          error={charRemaining < 0}
+          multiline
+          label="Bio"
+          name="bio"
+          value={formData.bio}
+          aria-describedby="bio"
+          onChange={handleChange}
         />
         <FormHelperText>{charRemaining} characters remaining</FormHelperText>
       </FormControl>
@@ -64,29 +71,29 @@ export default function AddPupForm({ user }) {
           Gender
         </FormLabel>
         <RadioGroup
-        value={formData.gender}
-        name="gender"
-        onChange={handleChange}
-        aria-label="gender"
-        row
+          value={formData.gender}
+          name="gender"
+          onChange={handleChange}
+          aria-label="gender"
+          row
         >
           <FormControlLabel
-          value="female"
-          control={<Radio />}
-          label="female"
-          labelPlacement="start"
-          style={{
-            color: "black"
-          }}
+            value="female"
+            control={<Radio />}
+            label="female"
+            labelPlacement="start"
+            style={{
+              color: "black",
+            }}
           />
           <FormControlLabel
-          value="male"
-          control={<Radio />}
-          label="male"
-          labelPlacement="start"
-          style={{
-            color: "black"
-          }}
+            value="male"
+            control={<Radio />}
+            label="male"
+            labelPlacement="start"
+            style={{
+              color: "black",
+            }}
           />
         </RadioGroup>
       </FormControl>
@@ -94,34 +101,34 @@ export default function AddPupForm({ user }) {
         Energy
       </InputLabel>
       <Slider
-      defaultValue={1}
-      id="energy"
-      name="energy"
-      onChange={handleEnergyChange}
-      aria-labelledy="discrete-sldier-custom"
-      step={1}
-      valueLabelDisplay="auto"
-      min={1}
-      max={5}
+        defaultValue={1}
+        id="energy"
+        name="energy"
+        onChange={handleEnergyChange}
+        aria-labelledby="discrete-sldier-custom"
+        step={1}
+        valueLabelDisplay="auto"
+        min={1}
+        max={5}
       />
 
       <MuiPickersUtilsProvider utils={MomentUtils}>
         <KeyboardDatePicker
-        disableToolbar
-        variant="inline"
-        value={selectedDate}
-        name="date"
-        format="MM/DD/yyyy"
-        margin="normal"
-        id="date-picker-inline"
-        label="Birthday"
-        onChange={handleDateChange}
-        KeyboardButtonProps={{
-          "aria-label": "change date",
-        }}
+          disableToolbar
+          variant="inline"
+          value={selectedDate}
+          name="date"
+          format="MM/DD/yyyy"
+          margin="normal"
+          id="date-picker-inline"
+          label="Birthday"
+          onChange={handleDateChange}
+          KeyboardButtonProps={{
+            "aria-label": "change date",
+          }}
         />
       </MuiPickersUtilsProvider>
       <Button type="submit"></Button>
-      </form>
+    </form>
   );
 }
