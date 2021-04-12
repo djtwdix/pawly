5f9b002deb4b0b929c9243a00d50f297d77b36ce
djtwdix <djtwd.ix@icloud.com>
2021-04-11 23:39:20 -0300

    Render only pup form

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,13 @@
 import SignIn from "./components/SignIn";
 
-import Navbar from './components/Navbar';
-import AddPupForm from './components/AddPupForm';
-
-
+import Navbar from "./components/Navbar";
+import AddPupForm from "./components/AddPupForm";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <SignIn />
       <AddPupForm />
-    
-      {/* <SignIn /> */}
     </div>
   );
 }
