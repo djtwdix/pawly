2ff213756751fce62008321fe10cd966c97f93bc
Catherine Dawe <dawe.cm@gmail.com>
2021-04-12 14:13:54 -0400

    add Pups component

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,15 @@ import "./stylesheets/App.scss"
 import SignIn from "./components/SignIn";
 import Navbar from "./components/Navbar";
 import AddPupForm from "./components/AddPupForm";
+import Pups from "./components/Pups"
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <AddPupForm />
+     {/* <AddPupForm /> */}
+     <Pups />
+
     </div>
   );
 }
