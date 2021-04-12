7ad49fada48290844652c48b2b402056d89b0dd6
Catherine Dawe <dawe.cm@gmail.com>
2021-04-11 20:56:27 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import SignIn from "./components/SignIn";
 import Navbar from './components/Navbar';
+import AddPupForm from './components/AddPupForm';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <SignIn />
+      <AddPupForm />
     </div>
   );
 }
