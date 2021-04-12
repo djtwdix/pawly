eae7270f1a500edf204c604a4f4cec7373a33e16
djtwdix <djtwd.ix@icloud.com>
2021-04-12 00:58:33 -0300

    Style pupform with muiTheme and add gender icons

--- client/src/components/AddPupForm.js
+++ client/src/components/AddPupForm.js
@@ -18,7 +18,12 @@ import {
 import MomentUtils from "@date-io/moment";
 import usePupData from "../hooks/usePupData";
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVenus, faMars } from "@fortawesome/free-solid-svg-icons";
 import "../stylesheets/AddPupForm.scss";
+
+import { muiTheme } from "../stylesheets/muiTheme";
+import { ThemeProvider } from "@material-ui/styles";
 
 //Component for for adding new pup
 export default function AddPupForm({ user }) {
@@ -32,114 +37,126 @@ export default function AddPupForm({ user }) {
     selectedDate,
   } = usePupData();
 
+  const male = <FontAwesomeIcon className="genderIcons" icon={faMars} />;
+  const female = <FontAwesomeIcon className="genderIcons" icon={faVenus} />;
+
   return (
     <div className="pupForm">
-      <form
-        className="pupForm__card"
-        onSubmit={(e) => {
-          addPup(e, user);
-        }}
-      >
-        <label class="custom-file-upload">
-          <input type="file" />
-          Upload Image
-        </label>
-        <FormControl>
-          <InputLabel htmlFor="name">Name</InputLabel>
-          <Input
-            name="name"
-            id="name"
-            value={formData.name}
-            aria-describedby="name"
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl>
-          <InputLabel htmlFor="breed">Breed</InputLabel>
-          <Input
-            type="text"
-            name="photoURL"
-            id="breed"
-            value={formData.breed}
-            aria-describedby="breed"
-            onChange={handleChange}
-          />
-        </FormControl>
-        <FormControl>
-          <TextField
-            error={charRemaining < 0}
-            multiline
-            label="Bio"
-            name="bio"
-            value={formData.bio}
-            aria-describedby="bio"
-            onChange={handleChange}
-          />
-          <FormHelperText>{charRemaining} characters remaining</FormHelperText>
-        </FormControl>
-        <FormControl component="fieldset">
-          <FormLabel className="pupForm__addPadding" component="legend">
-            Gender
-          </FormLabel>
-          <RadioGroup
-            name="gender"
-            onChange={handleChange}
-            aria-label="gender"
-            row
-          >
-            <FormControlLabel
-              value="female"
-              control={<Radio />}
-              label="female"
-              labelPlacement="start"
-              style={{
-                color: "black",
-              }}
+      <ThemeProvider theme={muiTheme}>
+        <form
+          className="pupForm__card"
+          onSubmit={(e) => {
+            addPup(e, user);
+          }}
+        >
+          <label class="pupForm__button">
+            <input type="file" />
+            Upload Image
+          </label>
+          <FormControl>
+            <InputLabel htmlFor="name">Name</InputLabel>
+            <Input
+              name="name"
+              id="name"
+              value={formData.name}
+              aria-describedby="name"
+              onChange={handleChange}
             />
-            <FormControlLabel
-              value="male"
-              control={<Radio />}
-              label="male"
-              labelPlacement="start"
-              style={{
-                color: "black",
-              }}
+          </FormControl>
+          <FormControl>
+            <InputLabel htmlFor="breed">Breed</InputLabel>
+            <Input
+              type="text"
+              name="photoURL"
+              id="breed"
+              value={formData.breed}
+              aria-describedby="breed"
+              onChange={handleChange}
             />
-          </RadioGroup>
-        </FormControl>
-        <InputLabel className="pupForm__addPadding" htmlFor="energy" mt="3">
-          Energy
-        </InputLabel>
-        <Slider
-          defaultValue={1}
-          id="energy"
-          name="energy"
-          onChange={handleEnergyChange}
-          aria-labelledby="discrete-sldier-custom"
-          step={1}
-          valueLabelDisplay="auto"
-          min={1}
-          max={5}
-        />
+          </FormControl>
+          <FormControl>
+            <TextField
+              error={charRemaining < 0}
+              multiline
+              label="Bio"
+              name="bio"
+              value={formData.bio}
+              aria-describedby="bio"
+              onChange={handleChange}
+            />
+            <FormHelperText>
+              <span class={charRemaining < 0 && "pupForm__red"}>
+                {charRemaining} characters remaining
+              </span>
+            </FormHelperText>
+          </FormControl>
+          <FormControl component="fieldset">
+            <FormLabel className="pupForm__addPadding" component="legend">
+              Gender
+            </FormLabel>
+            <RadioGroup
+              name="gender"
+              onChange={handleChange}
+              aria-label="gender"
+              row
+            >
+              <div className="pupForm__genderGroup">
+                <FormControlLabel
+                  value="female"
+                  control={<Radio />}
+                  label={female}
+                  labelPlacement="start"
+                  style={{
+                    color: "black",
+                  }}
+                />
+                <FormControlLabel
+                  value="male"
+                  control={<Radio />}
+                  label={male}
+                  labelPlacement="start"
+                  style={{
+                    color: "black",
+                  }}
+                />
+              </div>
+            </RadioGroup>
+          </FormControl>
+          <InputLabel className="pupForm__addPadding" htmlFor="energy" mt="3">
+            Energy
+          </InputLabel>
 
-        <MuiPickersUtilsProvider utils={MomentUtils}>
-          <KeyboardDatePicker
-            disableToolbar
-            variant="inline"
-            value={selectedDate}
-            name="date"
-            format="MM/DD/yyyy"
-            margin="normal"
-            id="date-picker-inline"
-            label="Birthday"
-            onChange={handleDateChange}
-            KeyboardButtonProps={{
-              "aria-label": "change date",
-            }}
+          <Slider
+            defaultValue={1}
+            id="energy"
+            name="energy"
+            onChange={handleEnergyChange}
+            aria-labelledby="discrete-sldier-custom"
+            step={1}
+            valueLabelDisplay="auto"
+            min={1}
+            max={5}
           />
-        </MuiPickersUtilsProvider>
-        <Button type="submit"></Button>
-      </form>
+
+          <MuiPickersUtilsProvider utils={MomentUtils}>
+            <KeyboardDatePicker
+              disableToolbar
+              variant="inline"
+              value={selectedDate}
+              name="date"
+              format="MM/DD/yyyy"
+              margin="normal"
+              id="date-picker-inline"
+              label="Birthday"
+              onChange={handleDateChange}
+              KeyboardButtonProps={{
+                "aria-label": "change date",
+              }}
+            />
+          </MuiPickersUtilsProvider>
+          <Button type="submit">Submit</Button>
+        </form>
+      </ThemeProvider>
     </div>
   );
 }
