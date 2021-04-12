c03d030d274840913a3173c771d63319dcb0d4f2
djtwdix <djtwd.ix@icloud.com>
2021-04-12 16:53:25 -0300

    Import react router and begin implementing

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,12 @@ import { auth } from "./firebase/config";
 import SignIn from "./components/SignIn";
 import Navbar from "./components/Navbar";
 import AddPupForm from "./components/AddPupForm";
-import PupsList from "./components/PupsList"
-import Pup from "./components/Pup"
+import PupsList from "./components/PupsList";
+import Pup from "./components/Pup";
+import ChatList from "./components/ChatList";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import CardStack from "./components/CardStack";
 
 function App() {
   const [user, loading, error] = useAuthState(auth);
@@ -18,10 +22,18 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
-     {/* <AddPupForm /> */}
-     <Pup />
-
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Navbar user={user} />
+            {user ? <CardStack /> : <SignIn />}
+          </Route>
+          <Route exact path="/chats">
+            <Navbar />
+            <ChatList />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../stylesheets/Navbar.scss";
 import logo from "../assets/images/pawlylogo.png";
+import { Link, useHistory } from "react-router-dom";
 
 //Icons
 import PersonIcon from "@material-ui/icons/Person";
@@ -8,37 +9,47 @@ import ForumIcon from "@material-ui/icons/Forum";
 import ArrowBackIosIcon from "@material-ui/icons/ArrowBackIos";
 import { Avatar, IconButton } from "@material-ui/core";
 
-export default function Navbar({ user }) {
-  const backButton = true;
+export default function Navbar({ user, backButton }) {
+  const history = useHistory();
+
   return (
     <nav className="navbar">
       {backButton ? (
         <IconButton>
-          <ArrowBackIosIcon fontSize="large" />
+          <ArrowBackIosIcon
+            fontSize="large"
+            onClick={(e) => history.goBack()}
+          />
         </IconButton>
       ) : user ? (
-        <IconButton>
-          <Avatar
-            className="navbar__avatar"
-            src={user.photoURL}
-            style={{
-              margin: "0",
-              height: "49px",
-              width: "49px",
-            }}
-          >
-            DT
-          </Avatar>
-        </IconButton>
+        <Link to="users/:uid">
+          <IconButton>
+            <Avatar
+              className="navbar__avatar"
+              src={user.photoURL}
+              style={{
+                margin: "0",
+                height: "49px",
+                width: "49px",
+              }}
+            >
+              DT
+            </Avatar>
+          </IconButton>
+        </Link>
       ) : (
         <IconButton>
           <PersonIcon fontSize="large" />
         </IconButton>
       )}
-      <img className="navbar__logo" src={logo} alt="pawly-logo"></img>
-      <IconButton>
-        <ForumIcon fontSize="large" />
-      </IconButton>
+      <Link to="/">
+        <img className="navbar__logo" src={logo} alt="pawly-logo"></img>
+      </Link>
+      <Link to="/chats">
+        <IconButton>
+          <ForumIcon fontSize="large" />
+        </IconButton>
+      </Link>
     </nav>
   );
 }
