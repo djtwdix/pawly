7acb5f19e6776a4e607aae3dc3b53ffccb8991da
djtwdix <djtwd.ix@icloud.com>
2021-04-13 09:58:43 -0300

    Remove unnecessary exports from components index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,6 @@
 export { default as Navbar } from "./Navbar";
 export { default as ListItemContainer } from "./ListItemContainer";
 export { default as SignIn } from "./SignIn";
-export { default as ChatListItem } from "./ChatListItem";
 export { default as ChatList } from "./ChatList";
 export { default as ChatWindow } from "./ChatWindow";
 export { default as ProfileList } from "./ProfileList";
@@ -9,4 +8,3 @@ export { default as PupForm } from "./PupForm";
 export { default as PupCard } from "./PupCard";
 export { default as PupList } from "./PupList";
 export { default as CardStack } from "./PupCardStack";
-export { default as PupListItem } from "./PupListItem";
