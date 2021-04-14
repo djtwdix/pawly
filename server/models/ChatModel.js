fb7cb399e5b0a4790dc3ff0e67bf236efc0ec1f4
djtwdix <djtwd.ix@icloud.com>
2021-04-14 00:05:57 -0300

    Add last message to chat model

--- server/models/ChatModel.js
+++ server/models/ChatModel.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 const chatModel = mongoose.Schema({
   participants: Array,
   created_at: { type: Date, default: Date.now() },
+  last_message: Array,
 });
 
 export default mongoose.model("chats", chatModel);
