83c37fd31fc76ffbc76abfb8b4fb3649841ce697
Jesse <jessedaniel@live.ca>
2021-04-14 14:58:22 -0600

    conditionally render pupcard or infocard

--- client/src/components/PupCardStack.js
+++ client/src/components/PupCardStack.js
@@ -1,18 +1,25 @@
 import React from "react";
 import usePupData from "../hooks/usePupData";
 import PupCard from "./PupCard";
+import PupCardInfo from "./PupCardInfo";
 import SwipeButtons from "./SwipeButtons";
+import useCardActions from "../hooks/useCardActions";
 
 export default function PupCardStack({ user }) {
   const { pups } = usePupData();
+  const { showPhoto, photoController } = useCardActions();
   
   const parsedPups = pups.map((pup) => {
-    return <PupCard key={pup._id} pup={pup} user={user} owner={pup.owner_id} />;
+    return <PupCard key={pup._id} pup={pup} user={user} owner={pup.owner_id} photoController={photoController} />;
+  });
+
+  const parsedPupsInfo = pups.map((pup) => {
+    return <PupCardInfo key={pup._id} pup={pup} user={user} owner={pup.owner_id} photoController={photoController} />;
   });
 
   return (
     <section>
-      <div className="card__container">{parsedPups}</div>
+      <div className="card__container">{ showPhoto ? parsedPups : parsedPupsInfo }</div>
       <SwipeButtons />
     </section>
   );
