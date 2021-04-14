41d42af258fbca5a5b17cd247ad586badfcc53d3
djtwdix <djtwd.ix@icloud.com>
2021-04-14 14:08:01 -0300

    Remove useEffect and getPupsByOwnerId

--- client/src/components/PupList.js
+++ client/src/components/PupList.js
@@ -1,19 +1,12 @@
 import { Add } from "@material-ui/icons";
-import React, { useEffect } from "react";
+import React from "react";
 import ListItemContainer from "./ListItemContainer";
 import PupListItem from "./PupListItem";
 import { Link } from "react-router-dom";
 import usePupData from "../hooks/usePupData";
 
 export default function PupList({ user }) {
-  const { getPupsByOwnerId, userPups } = usePupData();
-  
-
-  useEffect(() => {
-    if (user) {
-      getPupsByOwnerId(user.uid)
-    }
-  });
+  const { userPups } = usePupData();
 
   const parsedPups = userPups.map((pup) => {
     return <PupListItem key={pup._id} pup={pup} />;
