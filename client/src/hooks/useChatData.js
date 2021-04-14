e86b508b79964d40d0a3e29fce944a42ffbbc38d
djtwdix <djtwd.ix@icloud.com>
2021-04-14 00:07:27 -0300

    Add /messages to the getMessageByChatId url

--- client/src/hooks/useChatData.js
+++ client/src/hooks/useChatData.js
@@ -4,14 +4,14 @@ import { useState } from "react";
 export default function useChatData() {
   const [chats, setChats] = useState([]);
 
-  const getChatsByUserID= async (userId) => {
-     const result = await axios.post("/chats/all", { user_id: userId });
-     setChats(result.data);
-  }
+  const getChatsByUserID = async (userId) => {
+    const result = await axios.post("/chats/all", { user_id: userId });
+    setChats(result.data);
+  };
 
   const getMessagesByChatId = (chatId) => {
-    return axios.get(`/chats/${chatId}/`);
-  }
+    return axios.get(`/chats/${chatId}/messages`);
+  };
 
   return { getChatsByUserID, getMessagesByChatId, chats };
 }
