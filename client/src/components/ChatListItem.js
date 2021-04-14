a06e6a29d4c1051df1581bc11fc4b316df990d6b
djtwdix <djtwd.ix@icloud.com>
2021-04-14 00:13:59 -0300

    Implement last message and moment for timestamp

--- client/src/components/ChatListItem.js
+++ client/src/components/ChatListItem.js
@@ -2,35 +2,44 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Avatar from "@material-ui/core/Avatar";
 import ListItemContainer from "./ListItemContainer";
-import useUserData from "../hooks/useUserData"
+import useUserData from "../hooks/useUserData";
+import moment from "moment";
 
 export default function Chat({ chat, user }) {
-  const [otherUser, setOtherUser] = useState({})
-  const { getUserById } = useUserData()
-   
- useEffect(() => {
-     if (chat && user) {
-       const otherUserID = chat.participants.filter((participant) => {
-         return participant !== user.uid
-        })
-       getUserById(otherUserID[0]).then(res => {
-          setOtherUser(res.data)
-        })
-     }
- }, [chat, user])
-  
+  const [otherUser, setOtherUser] = useState({});
+  const { getUserById } = useUserData();
+
+  const last_message = chat.last_message[0];
+
+  useEffect(() => {
+    if (chat && user) {
+      const otherUserID = chat.participants.filter((participant) => {
+        return participant !== user.uid;
+      });
+      getUserById(otherUserID[0]).then((res) => {
+        setOtherUser(res.data);
+      });
+    }
+  }, [chat, user]);
+
   return (
     <Link to={`/chats/${chat._id}`}>
-        <ListItemContainer>
-          <section className="chat">
-            <Avatar className="chat__image" alt={otherUser.name} src={otherUser.photoURL}></Avatar>
-            <div className="chat__details">
-              <h1>{otherUser.name}</h1>
-              <p>Hello!</p>
-            </div>
-            <p className="chat__timestamp">{chat.created_at}</p>
-          </section>
-        </ListItemContainer>
+      <ListItemContainer>
+        <section className="chat">
+          <Avatar
+            className="chat__image"
+            alt={otherUser.name}
+            src={otherUser.photoURL}
+          ></Avatar>
+          <div className="chat__details">
+            <h1>{otherUser.name}</h1>
+            <p>{last_message.text}</p>
+          </div>
+          <p className="chat__timestamp">
+            {moment(last_message.created_at).fromNow()}
+          </p>
+        </section>
+      </ListItemContainer>
     </Link>
   );
 }
