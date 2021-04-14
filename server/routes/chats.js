e19841e2a720a518cf7321ae02b3e5ad822e4063
djtwdix <djtwd.ix@icloud.com>
2021-04-14 00:05:36 -0300

    Add update last message and get chat by id routes

--- server/routes/chats.js
+++ server/routes/chats.js
@@ -2,12 +2,16 @@ import express from "express";
 import {
   createChat,
   getChatsByUserId,
+  updateLastMessage,
+  getChatById,
 } from "../controllers/ChatControllers.js";
 import { getMessagesByChatId } from "../controllers/MessageControllers.js";
 const router = express.Router();
 
 router.post("/", createChat);
+router.put("/", updateLastMessage);
 router.post("/all", getChatsByUserId);
-router.get("/:chatId", getMessagesByChatId);
+router.get("/:chatId/messages", getMessagesByChatId);
+router.get("/:chatId", getChatById);
 
 export { router as chatRoutes };
