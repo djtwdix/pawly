5688b4fcd89ae060d2dd29ac12813610f5938b0b
djtwdix <djtwd.ix@icloud.com>
2021-04-14 00:10:12 -0300

    Override capitalized letters on list items

--- client/src/components/ListItemContainer.js
+++ client/src/components/ListItemContainer.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Button } from "@material-ui/core";
 
-export default function ListItemContainer( { children, signOut }) {
+export default function ListItemContainer({ children, signOut }) {
   return (
     <div className="listItemContainer">
-      <Button className="listItemContainer" onClick={signOut ? signOut: undefined}> { children }</Button>
+      <Button
+        className="listItemContainer"
+        style={{ textTransform: "none" }}
+        onClick={signOut ? signOut : undefined}
+      >
+        {" "}
+        {children}
+      </Button>
     </div>
   );
 }
