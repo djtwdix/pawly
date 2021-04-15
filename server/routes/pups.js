c5cf720858ebed2cf280b3948721e458f81a93ba
Jesse <jessedaniel@live.ca>
2021-04-15 13:29:47 -0600

    add edit pup route

--- server/routes/pups.js
+++ server/routes/pups.js
@@ -3,11 +3,13 @@ import {
   createPup,
   getAllPups,
   getPupById,
+  editPup
 } from "../controllers/PupControllers.js";
 const router = express.Router();
 
 router.post("/", createPup);
 router.post("/all", getAllPups);
 router.get("/:pupId", getPupById);
+router.put("/:pupId", editPup)
 
 export { router as pupsRoutes };
