08d02e420841112fcb7c726654758037879dfe7b
djtwdix <djtwd.ix@icloud.com>
2021-04-15 15:51:12 -0300

    Add colours to energy icon corresponding to level

--- client/src/components/EnergyIcon.js
+++ client/src/components/EnergyIcon.js
@@ -1,17 +1,36 @@
 import React from "react";
-import BatteryCharging20RoundedIcon from '@material-ui/icons/BatteryCharging20Rounded';
-import BatteryCharging50RoundedIcon from '@material-ui/icons/BatteryCharging50Rounded';
-import BatteryCharging80RoundedIcon from '@material-ui/icons/BatteryCharging80Rounded'
-import BatteryChargingFullRoundedIcon from '@material-ui/icons/BatteryChargingFullRounded';
+import BatteryCharging20RoundedIcon from "@material-ui/icons/BatteryCharging20Rounded";
+import BatteryCharging50RoundedIcon from "@material-ui/icons/BatteryCharging50Rounded";
+import BatteryCharging80RoundedIcon from "@material-ui/icons/BatteryCharging80Rounded";
+import BatteryChargingFullRoundedIcon from "@material-ui/icons/BatteryChargingFullRounded";
 
-
-export default function EnergyIcon({energy}) {
+export default function EnergyIcon({ energy }) {
   return (
     <div>
-      {energy === 1 && <BatteryCharging20RoundedIcon fontSize="large"/>}
-      {energy === 2 && <BatteryCharging50RoundedIcon fontSize="large"/>}
-      {energy === 3 && <BatteryCharging80RoundedIcon fontSize="large"/>}
-      {energy === 4 && <BatteryChargingFullRoundedIcon fontSize="large"/>}
+      {energy === 1 && (
+        <BatteryCharging20RoundedIcon
+          fontSize="large"
+          style={{ color: "red" }}
+        />
+      )}
+      {energy === 2 && (
+        <BatteryCharging50RoundedIcon
+          fontSize="large"
+          style={{ color: "yellow" }}
+        />
+      )}
+      {energy === 3 && (
+        <BatteryCharging80RoundedIcon
+          fontSize="large"
+          style={{ color: "yellowgreen" }}
+        />
+      )}
+      {energy === 4 && (
+        <BatteryChargingFullRoundedIcon
+          fontSize="large"
+          style={{ color: "lightgreen" }}
+        />
+      )}
     </div>
-  )
+  );
 }
