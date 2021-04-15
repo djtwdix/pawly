0f4dc9927afe8d9b9d49d0dfcc0f5457ab994d78
djtwdix <djtwd.ix@icloud.com>
2021-04-15 10:02:11 -0300

    Add classes, change elements for pup list item

--- client/src/components/PupListItem.js
+++ client/src/components/PupListItem.js
@@ -1,20 +1,26 @@
 import React from "react";
 import ListItemContainer from "./ListItemContainer";
-import { Avatar } from "@material-ui/core";
-import { Link } from "react-router-dom"
+import { Avatar, Button } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 export default function PupListItem({ pup }) {
   return (
-    <ListItemContainer>
-      <Link to={`/pups/${pup._id}`}>
-      <section key={pup._id} className="pupListItem">
-        <Avatar alt={pup.name} src={pup.photoURL}></Avatar>
-        <div className="pupListItem__details">
-          <h1>{pup.name}</h1>
-          <p>{pup.breed}</p>
-        </div>
-      </section>
-      </Link>
-    </ListItemContainer>
+    <Link to={`/pups/${pup._id}`}>
+      <Button className="pupListItem__button">
+        <section key={pup._id} className="pupListItem">
+          <div className="pupListItem__avatarDetails">
+          <Avatar
+            alt={pup.name}
+            className="pupListItem__avatar"
+            src={pup.photoURL}
+          ></Avatar>
+          <div className="pupListItem__details">
+            <h1>{pup.name}</h1>
+            <p>{pup.breed}</p>
+          </div>
+          </div>
+        </section>
+      </Button>
+    </Link>
   );
 }
