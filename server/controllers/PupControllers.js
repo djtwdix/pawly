34f4a5050d6a02a55e9033fac46600bd5b9e5bf4
Jesse <jessedaniel@live.ca>
2021-04-15 13:29:36 -0600

    add edit pup controller

--- server/controllers/PupControllers.js
+++ server/controllers/PupControllers.js
@@ -12,6 +12,19 @@ export const createPup = (req, res) => {
   });
 };
 
+export const editPup = (req, res) => {
+  const pupsInfo = req.body;
+  const pupID = req.params.pupId
+  Pups.findOneAndUpdate({_id: pupID}, pupsInfo, (err, data) => {
+    if (err) {
+      res.status(500).send(err);
+      console.log(err);
+    } else {
+      res.status(200).send(data);
+    }
+  });
+}
+
 export const getPupsByOwner = (req, res) => {
   const owner_id = req.params.userId;
   Pups.find({ owner_id: owner_id }, (err, data) => {
