a76ce54a854264920a4b4592546b5f455b886b08
Jesse <jessedaniel@live.ca>
2021-04-15 13:28:42 -0600

    add state prop drilling

--- client/src/components/PupListItem.js
+++ client/src/components/PupListItem.js
@@ -2,11 +2,13 @@ import React from "react";
 import ListItemContainer from "./ListItemContainer";
 import { Avatar } from "@material-ui/core";
 import { Link } from "react-router-dom"
+import { usePup, usePupUpdate } from "../context/PupContext";
 
 export default function PupListItem({ pup }) {
+  //const switchPup = usePupUpdate(pup);
   return (
     <ListItemContainer>
-      <Link to={`/pups/${pup._id}`}>
+      <Link to={{ pathname:`/pups/edit` , state: pup}}>
       <section key={pup._id} className="pupListItem">
         <Avatar alt={pup.name} src={pup.photoURL}></Avatar>
         <div className="pupListItem__details">
