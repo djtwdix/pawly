d98e826aef4b57b7cea5f53562a65984a9a62d7b
Jesse <jessedaniel@live.ca>
2021-04-16 13:27:07 -0600

    add match alert function add match alert prop

--- client/src/components/PupCardStack.js
+++ client/src/components/PupCardStack.js
@@ -1,4 +1,4 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import usePupData from "../hooks/usePupData";
 import PupCard from "./PupCard";
 import PupCardInfo from "./PupCardInfo";
@@ -6,15 +6,25 @@ import SwipeButtons from "./SwipeButtons";
 import useCardActions from "../hooks/useCardActions";
 import getDistanceByCoords from "../helpers/getDistanceByCoords";
 import useLocationData from "../hooks/useLocationData";
+import MatchAlert from "./MatchAlert";
 
 export default function PupCardStack({ user }) {
   const { pups, setPups } = usePupData();
   const { showPhoto, photoController } = useCardActions();
   const { location } = useLocationData();
+  const [ showMatchAlert, setShowMatchAlert ] = useState(false);
 
   /* const nearPups = pups.filter(
     (pup) => console.log(pup) getDistanceByCoords(pup.location.coordinates, location.coordinates) < 50 
   ); */
+
+  const matchAlert = () => {
+    if (showMatchAlert) {
+      setShowMatchAlert(false)
+    } else {
+      setShowMatchAlert(true);
+    }
+  }
 
   
   const removePup = () => {
@@ -31,6 +41,7 @@ export default function PupCardStack({ user }) {
         user={user}
         owner={pup.owner_id}
         photoController={photoController}
+        matchAlert={matchAlert}
       
       />
     );
@@ -45,6 +56,7 @@ export default function PupCardStack({ user }) {
         user={user}
         owner={pup.owner_id}
         photoController={photoController}
+        matchAlert={matchAlert}
       />
     );
   });
@@ -53,6 +65,7 @@ export default function PupCardStack({ user }) {
     <section>
       <div className="pupCard__container">
         {showPhoto ? parsedPups : parsedPupsInfo}
+        { showMatchAlert && <MatchAlert matchAlert={matchAlert}/>}
       </div>
     </section>
   );
