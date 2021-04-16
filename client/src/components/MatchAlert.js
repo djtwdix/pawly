2fa07e9431d3aa6f900dc1cf1d0f17e206b7925e
Jesse <jessedaniel@live.ca>
2021-04-16 13:25:47 -0600

    create match alert componet

--- client/src/components/MatchAlert.js
+++ client/src/components/MatchAlert.js
@@ -1,21 +1,25 @@
-import { Button } from '@material-ui/core'
+import { Button, IconButton } from '@material-ui/core'
 import React from 'react'
 import { Link, useHistory } from 'react-router-dom'
 
-export default function MatchAlert() {
+export default function MatchAlert({matchAlert}) {
   const history = useHistory();
 
   return (
     <div className="matchAlert">
+      <div className="matchAlert__text">
       <h1>You've found a new friend!</h1>
+      </div>
+      <div className="matchAlert__Buttons">
       <Link to="/chats">
-        <Button>
+        <Button className="matchAlert__Button1" size="large">
            Let's Chat!
     </Button>
       </Link>
-      <Button onClick={(e) => history.goBack()} >
+      <Button className="matchAlert__Button2" size="large" onClick={(e) => matchAlert()} >
         Keep Swiping!
     </Button>
+    </div>
     </div>
   )
 }
