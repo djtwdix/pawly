9d8068d231fc5d4c37d2696fb37a1e0821420c5f
Catherine Dawe <dawe.cm@gmail.com>
2021-04-16 15:44:06 -0400

    fix the models for the timestamp

--- server/models/ChatModel.js
+++ server/models/ChatModel.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const chatModel = mongoose.Schema({
   participants: Array,
-  created_at: { type: Date, default: Date.now() },
+  //created_at: { type: Date, default: Date.now() },
   last_message: Array,
-});
+}, {timestamps: { createdAt: 'created_at' }});
 
 export default mongoose.model("chats", chatModel);
--- server/models/MessageModel.js
+++ server/models/MessageModel.js
@@ -3,8 +3,9 @@ import mongoose from "mongoose";
 const messageModel = mongoose.Schema({
   chat_id: String,
   sender_id: String,
-  text: String,
-  created_at: { type: Date, default: Date.now() },
-});
+  text: String
+  //created_at: { type: Date, default: Date.now() },
+}, {timestamps: { createdAt: 'created_at' }});
+
 
 export default mongoose.model("messages", messageModel);
