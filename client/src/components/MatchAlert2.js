499def012c307eab4478c0a6eaa718f1f65504ee
djtwdix <djtwd.ix@icloud.com>
2021-04-17 11:56:52 -0300

    Remove old text from math alert 2

--- client/src/components/MatchAlert2.js
+++ client/src/components/MatchAlert2.js
@@ -3,14 +3,13 @@ import { Link } from "react-router-dom";
 import matchAlert from "../assets/images/it's a.png";
 import dogBark from "../assets/audio/dog bark.wav";
 
-export default function MatchAlert2({}) {
+
+export default function MatchAlert2() {
   return (
     <div className="matchAlert2__container">
       <div className="matchAlert2">
         <Link to="/chats">
           <img src={matchAlert} alt="match"></img>
-          {/* <h1>It's a match!</h1>
-          <Button>click to chat</Button> */}
         </Link>
         <audio autoplay>
           <source src={dogBark} type="audio/mpeg" />
