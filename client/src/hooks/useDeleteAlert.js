108cc80d63e89290d4ac2e017b7203293676fbda
Jesse <jessedaniel@live.ca>
2021-04-17 11:05:58 -0600

    create delete alert hook

---
+++ client/src/hooks/useDeleteAlert.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+export default function useDeleteAlert() {
+  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
+
+
+  const deleteAlert = () => {
+ if (showDeleteAlert) {
+   setShowDeleteAlert(false);
+ } else {
+   setShowDeleteAlert(true);
+ }
+}; 
+
+return { deleteAlert , showDeleteAlert};
+}
