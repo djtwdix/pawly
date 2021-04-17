d5ffe535decc7173c3221c072db2dcc2ad2896c6
Jesse <jessedaniel@live.ca>
2021-04-17 17:24:48 -0600

    add bio route

--- server/routes/users.js
+++ server/routes/users.js
@@ -4,6 +4,7 @@ import {
   updateUser,
   getUserById,
   addLike,
+  addBio
 } from "../controllers/UserControllers.js";
 import { getPupsByOwner } from "../controllers/PupControllers.js";
 const router = express.Router();
@@ -13,5 +14,6 @@ router.get("/:userId/pups", getPupsByOwner);
 router.put("/:userId", updateUser);
 router.get("/:userId", getUserById);
 router.put("/:userId/likes", addLike);
+router.put("/:userId/bio", addBio);
 
 export { router as userRoutes };
