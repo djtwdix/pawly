4d3c0d60fd7eb4ffd1c63bf1b71437c828113218
djtwdix <djtwd.ix@icloud.com>
2021-04-17 22:31:32 -0300

    Add welcome image to login

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -2,6 +2,7 @@ import React from "react";
 import firebase from "firebase/app";
 import { auth } from "../firebase/config.js";
 import axios from "axios";
+import welcome from "../assets/images/4.png";
 
 import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
@@ -15,6 +16,7 @@ export default function SignIn({ location }) {
     // We will display Google and Facebook as auth providers.
     signInOptions: [
       firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     ],
     callbacks: {
@@ -32,6 +34,7 @@ export default function SignIn({ location }) {
   return (
     <div className="signInForm">
       <div className="signInForm__card">
+        <img src={welcome} alt="welcome" style={{ height: "150px" }} />
         <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth} />
       </div>
     </div>
