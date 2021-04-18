515652599bef3464eeadfa61d7ccdce98e52403a
djtwdix <djtwd.ix@icloud.com>
2021-04-18 12:21:50 -0300

    Style user profile

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -1,63 +1,75 @@
-import React from 'react'
+import React from "react";
 import useUserData from "../hooks/useUserData";
 import * as mui from "@material-ui/core";
 import getInitials from "../helpers/getInitials";
 import { Avatar } from "@material-ui/core";
 
-
-
 export default function UserProfile() {
-  const { handleChange, addBio, charRemaining, bio, name, email, photo } = useUserData();
+  const {
+    handleChange,
+    addBio,
+    charRemaining,
+    bio,
+    name,
+    email,
+    photo,
+  } = useUserData();
 
   return (
-    <span className="userBio">
-      <div className="userBio__display">
-        <Avatar
-          className="userBio__avatar"
-          src={photo}
-          style={{
-            margin: "0",
-            height: "110px",
-            width: "110px",
-          }}
-        >
-          {/* {getInitials(name)} */}
-        </Avatar>
+    <section className="userBio__container">
+      <div className="userBio">
+        <div className="userBio__display">
+          <Avatar
+            className="userBio__avatar"
+            src={photo}
+            style={{
+              margin: "0",
+              position: "relative",
+              top: "40px",
+              right: "20px",
+              height: "110px",
+              width: "110px",
+            }}
+          >
+            {/* {getInitials(name)} */}
+          </Avatar>
+        </div>
         <div className="userBio__info">
           <h1>{name}</h1>
-          <h1>{email}</h1>
+          <h4>{email}</h4>
         </div>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            addBio();
+          }}
+        >
+          <div className="userBio__form">
+            <mui.FormControl>
+              <mui.TextField
+                error={charRemaining < 0}
+                multiline
+                label="Bio"
+                name="bio"
+                value={bio}
+                aria-describedby="bio"
+                onChange={handleChange}
+                required={true}
+              />
+              <mui.FormHelperText>
+                <span
+                  className={charRemaining < 0 ? "pupForm__red" : undefined}
+                >
+                  {charRemaining} characters remaining
+                </span>
+              </mui.FormHelperText>
+            </mui.FormControl>
+            <mui.Button style={{ color: " #fd93ff" }} type="submit">
+              Submit
+            </mui.Button>
+          </div>
+        </form>
       </div>
-      <form onSubmit={(e) => {
-        e.preventDefault()
-        addBio()
-      }}>
-        <div className="userBio__form" >
-          <mui.FormControl>
-            <mui.TextField
-              error={charRemaining < 0}
-              multiline
-              label="Bio"
-              name="bio"
-              value={bio}
-              aria-describedby="bio"
-              onChange={handleChange}
-              required={true}
-            />
-            <mui.FormHelperText>
-              <span className={charRemaining < 0 ? "pupForm__red" : undefined}>
-                {charRemaining} characters remaining
-          </span>
-            </mui.FormHelperText>
-          </mui.FormControl>
-          <mui.Button
-            style={{ backgroundColor: "rgb(176, 176, 176)", color: "white" }}
-            type="submit"
-          >
-            Submit
-          </mui.Button>
-        </div>
-      </form>
-    </span>
-  )
+    </section>
+  );
 }
--- client/src/hooks/useUserData.js
+++ client/src/hooks/useUserData.js
@@ -6,49 +6,42 @@ import firebase from "firebase/app";
 
 export default function useUserData() {
   const user = auth.currentUser;
-  const [charRemaining, setCharRemaining] = useState(140);
+  const [charRemaining, setCharRemaining] = useState(240);
   const [bio, setBio] = useState("");
-  const [name, setName] = useState("")
-  const [email, setEmail] = useState("")
-  const [photo, setPhoto] = useState("")
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [photo, setPhoto] = useState("");
 
   const addBio = () => {
     console.log(bio);
     axios.put(`/users/${user.uid}/bio`, {
-      bio: bio
+      bio: bio,
     });
-  }
+  };
 
   useEffect(() => {
     if (user) {
-      getUserById(user.uid)
-        .then((res) => {
-         const currentUser = res.data
-          const currentBio = currentUser.bio;
-          setBio(currentBio)
-          console.log('current bio:', currentBio);
-          setCharRemaining(140 - bio.length);
-        }
-        )
+      getUserById(user.uid).then((res) => {
+        const currentUser = res.data;
+        const currentBio = currentUser.bio;
+        setBio(currentBio);
+      });
     }
-  }, [user])
-
+  }, [user]);
 
   const handleChange = (e) => {
     setBio(e.target.value);
     if (e.target.name === "bio") {
-      setCharRemaining(140 - e.target.value.length);
+      setCharRemaining(240 - e.target.value.length);
     }
-
-    console.log(bio);
   };
 
-  firebase.auth().onAuthStateChanged(function(user) {
+  firebase.auth().onAuthStateChanged(function (user) {
     if (user != null) {
-       setName(user.displayName);
-       setEmail(user.email);
-       setPhoto(user.photoURL);
-    } 
+      setName(user.displayName);
+      setEmail(user.email);
+      setPhoto(user.photoURL);
+    }
   });
 
   return { addBio, handleChange, bio, name, email, photo, charRemaining };
