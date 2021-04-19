b69b5aa514643208391d6c4fa85270bd15cfafee
Jesse <jessedaniel@live.ca>
2021-04-19 11:48:51 -0600

    clarified purpose of use effect

--- client/src/hooks/useLocationData.js
+++ client/src/hooks/useLocationData.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export default function useLocationData() {
   const [location, setLocation] = useState(null);
 
+  //pulls location info from browser, to be used in location rendering, saved as state
   useEffect(() => {
     navigator.geolocation.getCurrentPosition((res) => {
       setLocation({
