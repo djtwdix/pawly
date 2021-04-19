aaf698f7201e586145567c2d5e22538cb0cba182
Jesse <jessedaniel@live.ca>
2021-04-19 11:49:00 -0600

    clarified purpose of code

--- client/src/hooks/useProfileData.js
+++ client/src/hooks/useProfileData.js
@@ -4,8 +4,11 @@ import getUserById from "../helpers/getUserById";
 
 export default function useProfileData() {
   const [profile, setUserProfile] = useState({});
+
+  //pulls params from url
   const params = useParams();
 
+  //fetches user based on params of url and sets current user profile to result data
   useEffect(() => {
     getUserById(params.id).then((res) => {
       setUserProfile(res.data);
