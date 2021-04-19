01a7456b9a8c82b9dba09ad985d2f1f7803856cb
Jesse <jessedaniel@live.ca>
2021-04-19 11:48:38 -0600

    clarified where hook is used

--- client/src/hooks/useDeleteAlert.js
+++ client/src/hooks/useDeleteAlert.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function useDeleteAlert() {
   const [showDeleteAlert, setShowDeleteAlert] = useState(false);
 
+  //toggles whether delete alert is show upon clicking destroy button, saved as state
   const deleteAlert = () => {
     if (showDeleteAlert) {
       setShowDeleteAlert(false);
