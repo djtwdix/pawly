73d5a044c7ebcf2a11cdfb426bda694c84864bae
djtwdix <djtwd.ix@icloud.com>
2021-04-19 02:18:11 -0300

    Remove auth data import

--- client/src/hooks/useProfileData.js
+++ client/src/hooks/useProfileData.js
@@ -1,15 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import getUserById from "../helpers/getUserById";
-import { auth } from "../firebase/config";
 
 export default function useProfileData() {
-  const user = auth.currentUser;
   const [profile, setUserProfile] = useState({});
   const params = useParams();
 
   useEffect(() => {
-    
     getUserById(params.id).then((res) => {
       setUserProfile(res.data);
     });
