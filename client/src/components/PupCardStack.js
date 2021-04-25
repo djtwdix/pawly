c34cd346715e66d4b8ec57eec9f36b2900d70701
djtwdix <djtwd.ix@icloud.com>
2021-04-25 13:45:16 -0300

    Remove parsing and other functions from component

--- client/src/components/PupCardStack.js
+++ client/src/components/PupCardStack.js
@@ -1,65 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import usePupData from "../hooks/usePupData";
-import PupCard from "./PupCard";
-import PupCardInfo from "./PupCardInfo";
 import loadingGif from "../assets/images/giphy.gif";
 import allDone from "../assets/images/alldone.png";
-import useCardActions from "../hooks/useCardActions";
 import MatchAlert2 from "./MatchAlert2";
 
-export default function PupCardStack({ user, soundOff }) {
-  const { pups, setPups } = usePupData();
-  const { showPhoto, photoController } = useCardActions();
-  const [showMatchAlert, setShowMatchAlert] = useState(false);
-  const [loading, setLoading] = useState(true);
-  const [chat, setChat] = useState(null)
-
-  const setMatchAlertFalse = () => {
-    if (showMatchAlert) {
-      setShowMatchAlert(false);
-    }
-  };
-
-  const removePup = () => {
-    setPups((prev) => [...prev.slice(0, prev.length - 1)]);
-    parsedPups.slice(0, parsedPups.length - 1);
-    parsedPupsInfo.slice(0, parsedPups.length - 1);
-    setLoading(false);
-  };
-
-  const parsedPups = pups.map((pup, index) => {
-    return (
-      <PupCard
-        index={index}
-        removePup={removePup}
-        key={pup._id}
-        pup={pup}
-        user={user}
-        owner={pup.owner_id}
-        photoController={photoController}
-        setShowMatchAlert={setShowMatchAlert}
-        setChat={setChat}
-        pups={pups}
-      />
-    );
-  });
-
-  const parsedPupsInfo = pups.map((pup, index) => {
-    return (
-      <PupCardInfo
-        index={index}
-        removePup={removePup}
-        key={pup._id}
-        pup={pup}
-        user={user}
-        owner={pup.owner_id}
-        photoController={photoController}
-        setShowMatchAlert={setShowMatchAlert}
-        setChat={setChat}
-        pups={pups}
-      />
-    );
-  });
+export default function PupCardStack({ soundOff }) {
+  const {
+    parsedPups,
+    parsedPupsInfo,
+    showMatchAlert,
+    setMatchAlertFalse,
+    showPhoto,
+    chat,
+    loading,
+  } = usePupData();
 
   return (
     <section>
