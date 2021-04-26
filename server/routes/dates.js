ab5727444582ba45bccd7d0fecac18ace282521a
Jesse <jessedaniel@live.ca>
2021-04-26 14:10:02 -0600

    add get route

--- server/routes/dates.js
+++ server/routes/dates.js
@@ -1,10 +1,11 @@
 import express from "express";
 import {
-  createDate
+  createDate, getDatesForUser
 } from "../controllers/DateControllers.js";
 const router = express.Router();
 
 router.post("/", createDate);
+router.get("/", getDatesForUser);
 
 export { router as dateRoutes };
 
