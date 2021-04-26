e63750d1dc1129b4e4bc15d17db5574036283d25
Jesse <jessedaniel@live.ca>
2021-04-26 14:57:27 -0600

    add useEffect/set other user display

--- client/src/components/PlayDateListItem.js
+++ client/src/components/PlayDateListItem.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import getUserById from "../helpers/getUserById";
 
-export default function PlayDateListItem({date, otherUser}) {
+
+export default function PlayDateListItem({date, user, participants}) {
+  const [otherUser, setOtherUser] = useState({});
+  
+  useEffect(() => {
+    let mounted = true;
+    if (date && user) {
+      const otherUserID = participants.filter((participant) => {
+        return participant !== user.uid;
+      });
+      getUserById(otherUserID[0]).then((res) => {
+        if (mounted) {
+          setOtherUser(res.data);
+        }
+      });
+    }
+    return () => {
+      mounted = false;
+    };
+  }, [date, user]);
+
   return (
     <div>
       {date}
-      {otherUser}
+      {otherUser.name}
     </div>
   )
 }
