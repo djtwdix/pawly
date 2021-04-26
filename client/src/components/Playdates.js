c10ffe619189f5ec38ce0811f4c8512daece979c
Jesse <jessedaniel@live.ca>
2021-04-26 13:52:50 -0600

    parse dates for render

--- client/src/components/Playdates.js
+++ client/src/components/Playdates.js
@@ -2,10 +2,27 @@ import React from "react";
 import { Link } from "react-router-dom";
 import ListItemContainer from "./ListItemContainer";
 import { Add } from "@material-ui/icons";
+import useDateData from "../hooks/useDateData";
+import PlayDateListItem from "./PlayDateListItem";
 
 export default function Playdates() {
+  const { dates } = useDateData();
+
+  const parsedDates = dates.map((date) => {
+    return (
+      <section className="playDateListItem__container">
+          <PlayDateListItem 
+          date={date.date}
+          otherUser={date.otherUser}>
+          </PlayDateListItem>
+      </section>
+    );
+  });
+
+
   return (
     <section className="pupListItem__container">
+      {parsedDates}
       <Link to="/playdates">
         <ListItemContainer>
           <Add />
